ad91d97efd29f8ad3a48d7569840ed2dd5c1c100
Justin.wang <wangrongchao@126.com>
2018-02-07 16:04:14 +0800

    adjust file

--- DingDingRevokeMessagePod.podspec
+++ DingDingRevokeMessagePod.podspec
@@ -9,10 +9,10 @@ Pod::Spec.new do |spec|
   spec.homepage         = "https://github.com/AloneMonkey/MonkeyDevPod"   #Pod的地址
 
   spec.license          = { :type => "BSD", :file => "LICENSE" }          #License
-  spec.author           = { "AloneMonkey" => "liupeiqing1993@163.com" }   #作者
+  spec.author           = { "Justin.Wang" => "wangrongchao@126.com" }     #作者
   spec.social_media_url = "http://weibo.com/xiaoqing28"                   #weibo
   spec.platform         = :ios, "8.0"                                     #平台、版本
-  spec.source           = { :git => "https://github.com/AloneMonkey/MonkeyPod.git", :tag => spec.version.to_s }  #代码的git地址以及tag
+  spec.source           = { :git => "https://github.com/WJustin/DingDingRevokeMessagePod.git", :tag => spec.version.to_s }  #代码的git地址以及tag
   spec.source_files     = "DingDingRevokeMessagePod/**/*.{h,m}"        #本地验证，表示当前目录以及子目录的所有h或m结尾的文件   如果发布到MonkeyPodSpecs需要填写git clone下来的对应的路径
   spec.public_header_files = "DingDingRevokeMessagePod/DingDingRevokeMessagePod.h"        #需要对外导出的头文件  此处为本地验证
   spec.requires_arc     = true                                       #ARC
--- DingDingRevokeMessagePod/DingDingRevokeMessagePod.m
+++ DingDingRevokeMessagePod/DingDingRevokeMessagePod.m
@@ -11,14 +11,107 @@
 #import "DingDingRevokeMessagePod.h"
 #import <CaptainHook/CaptainHook.h>
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 
-CHDeclareClass(CustomViewController)
+@interface YYLabel
 
-CHOptimizedMethod(0, self, NSString*, CustomViewController,getMyName){
-    return @"MonkeyDevPod";
+@property (nonatomic) NSString *text;
+@property (nonatomic) NSAttributedString *attributedText;
+
+@end
+
+@interface WKBizMessage
+
+@property(nonatomic) BOOL isMine;
+@property(nonatomic) long long recallStatus;
+
+@end
+
+@interface DTBizMessage
+
+@property(nonatomic) long long senderId;
+@property(copy, nonatomic) NSString *localMid;
+@property(nonatomic) long long mId;
+@property(nonatomic) long long attachmentsType; //1
+@property(nonatomic) unsigned long long sendStatus; // 0
+@property(nonatomic) unsigned long long isDecrypt; // 0 正常 2废弃（你撤回了一条消息）
+@property(nonatomic) long long recallStatus;
+@property(nonatomic) long long createdTime;
+@property(copy, nonatomic) NSString *content;
+@property(nonatomic) _Bool isMine;
+
+@end
+
+@interface DTMessageControllerDataSource
+
+@property (nonatomic) NSArray<DTBizMessage *> *messages;
+
+@end
+
+@interface DTMessagePerformBatchManager
+
+@property (nonatomic) NSArray<DTBizMessage *> *messages;
+
+@end
+
+@interface DTMessageBaseViewController
+
+@property(retain, nonatomic) DTMessageControllerDataSource *dataSource;
+- (void)receivedMessageNoticeUpdateNotification:(id)arg1;
+- (void)viewWillAppear:(BOOL)arg1;
+
+@end
+
+CHDeclareClass(DTMessageControllerDataSource)
+CHDeclareClass(DTMessageBaseViewController)
+CHDeclareClass(YYLabel)
+
+CHOptimizedMethod1(self, void, DTMessageControllerDataSource, setMessages, NSArray<DTBizMessage *> *, messages) {
+    NSMutableArray *mutableArray = [[NSMutableArray alloc] init];
+    __block DTBizMessage *preMessage;
+    [messages enumerateObjectsUsingBlock:^(DTBizMessage * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj.recallStatus > 0) {
+            obj.recallStatus = 0;
+            DTBizMessage *bizMessage = [[objc_getClass("DTBizMessage") alloc] init];
+            bizMessage.senderId = obj.senderId;
+            bizMessage.mId = obj.mId;
+            bizMessage.isMine = obj.isMine;
+            bizMessage.attachmentsType = obj.attachmentsType;
+            bizMessage.content = obj.content;
+            bizMessage.recallStatus = 1;
+            if (preMessage.mId > 0 && preMessage.mId != obj.mId) {
+                [mutableArray addObject:obj];
+            }
+            [mutableArray addObject:bizMessage];
+        } else {
+            [mutableArray addObject:obj];
+        }
+        preMessage = obj;
+    }];
+    CHSuper1(DTMessageControllerDataSource, setMessages, mutableArray);
+}
+
+CHOptimizedMethod1(self, void, YYLabel, setAttributedText, NSAttributedString *, attributedText) {
+    if ([attributedText.string containsString:@"撤回了一条消息"] &&
+        ![attributedText.string containsString:@"已阻止"]) {
+        attributedText = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"已阻止%@", attributedText.string] attributes:nil];
+    }
+    CHSuper1(YYLabel, setAttributedText, attributedText);
+}
+
+CHOptimizedMethod1(self, void, DTMessageBaseViewController, receivedMessageNoticeUpdateNotification, id, arg1) {
+    //   NSNotification *notify = (NSNotification *)arg1;
+    //   WKBizMessage *message = [notify.userInfo[@"WKUserInfoMessagesKey"] firstObject];
+    //   message.recallStatus = 0;
+    //   CHSuper1(DTMessageBaseViewController, receivedMessageNoticeUpdateNotification, arg1);
 }
 
 CHConstructor{
-    CHLoadLateClass(CustomViewController);
-    CHClassHook(0, CustomViewController, getMyName);
+    CHLoadLateClass(YYLabel);
+    CHLoadLateClass(DTMessageControllerDataSource);
+    CHLoadLateClass(DTMessageBaseViewController);
+    CHHook1(DTMessageBaseViewController, receivedMessageNoticeUpdateNotification);
+    CHHook1(DTMessageControllerDataSource, setMessages);
+    CHHook1(YYLabel, setAttributedText);
 }
+
